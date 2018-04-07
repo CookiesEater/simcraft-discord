3fb7d9add77f0bdf7165706cf7c1a2d6119a5117
Alexey Shishkov <cookies.eater.03@gmail.com>
2018-04-07 22:02:56 +0300

    Add fail test simulation

--- src/__mocks__/child_process.js
+++ src/__mocks__/child_process.js
@@ -1,18 +1,24 @@
 const childProcess = {
-  spawn: jest.fn(),
-};
-childProcess.spawn.mockReturnValue({
-  on: (event, callback) => {
-    if (event === 'close') {
-      callback(0);
+  spawn: jest.fn((process, params) => {
+    let exitCode = 0;
+    if (params.indexOf('armory=eu,fail,fail') !== -1) {
+      exitCode = 1;
     }
-  },
-  stdout: {
-    on: jest.fn(),
-  },
-  stderr: {
-    on: jest.fn(),
-  },
-});
+
+    return {
+      on: (event, callback) => {
+        if (event === 'close') {
+          callback(exitCode);
+        }
+      },
+      stdout: {
+        on: jest.fn(),
+      },
+      stderr: {
+        on: jest.fn(),
+      },
+    }
+  }),
+};
 
 module.exports = childProcess;
